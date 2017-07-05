0d900fc76cfa4a30a671921b896074d939b8c56b
Ernesto Vargas <me@netoxico.com>
2017-07-04 19:51:47 -0500

    update readme

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from sg_search import SGSearch
 
-search = SGSearch('104.198.231.232:9202')
-results = search.find_meal('cabernet sauvignon, faust 11 napa', 'cabernet sauvignon')
+search = SGSearch('localhost:9200')
+result = search.find_meal('cabernet sauvignon, faust 11 napa', 'cabernet sauvignon')
 
-print results
+print(result)
